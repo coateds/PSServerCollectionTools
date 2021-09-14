5c6a6e31dc4fc960e15df71aa043054e9aa16f14
Dave Coate <coateds@outlook.com>
2021-09-13 21:02:49 -0700

    configure .psm1 file

--- PSServerCollectionTools/PSServerCollectionTools.psm1
+++ PSServerCollectionTools/PSServerCollectionTools.psm1
@@ -1,1 +1,29 @@
 # No Content Yet
+
+Set-StrictMode -Version Latest
+
+$PublicFunctions = Get-ChildItem -Path $PSScriptRoot\PublicFunctions\*.ps1
+
+foreach ($import in $PublicFunctions) {
+    try {
+        Write-Verbose "Importing $($import.FullName)"
+        . $import.FullName
+    } catch {
+        Write-Error "Failed to import function $($import.FullName): $_"
+    }
+}
+
+$PrivateFunctions = Get-ChildItem -Path $PSScriptRoot\PrivateFunctions\*.ps1
+
+foreach ($import in $PrivateFunctions) {
+    try {
+        Write-Verbose "Importing $($import.FullName)"
+        . $import.FullName
+    } catch {
+        Write-Error "Failed to import function $($import.FullName): $_"
+    }
+}
+
+foreach ($file in $PublicFunctions) {
+    Export-ModuleMember -Function $file.BaseName
+}
